c3f68c56a888100bb7deaf02824181c8b068a017
BrayanDev01 <dev01@grupocidade.com>
2025-04-05 14:14:56 -0300

    change color theme

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import router from './router'
 import PrimeVue from 'primevue/config';
 import Aura from '@primeuix/themes/aura';
 import 'primeicons/primeicons.css'
-
+import { definePreset } from '@primeuix/themes';
 
 import InputText from 'primevue/inputtext';
 import Button from 'primevue/button';
@@ -29,13 +29,31 @@ import SpeedDial from 'primevue/speeddial';
 // Parse.initialize("zFnXp8duZFOLBWfnqnj9hPbPcXd1YYBv7155jKyu", "FIFzV9EkueocfjksA9RmdFbPSYuP4WCHHFOFd9L0");
 // Parse.serverURL = "https://grupocidade.b4a.io/";
 
+const MyPreset = definePreset(Aura, {
+    semantic: {
+        primary: {
+            50: '{blue.50}',
+            100: '{blue.100}',
+            200: '{blue.200}',
+            300: '{blue.300}',
+            400: '{blue.400}',
+            500: '{blue.500}',
+            600: '{blue.600}',
+            700: '{blue.700}',
+            800: '{blue.800}',
+            900: '{blue.900}',
+            950: '{blue.950}'
+        }
+    }
+});
+
 
 const app = createApp(App)
 
 
 app.use(PrimeVue, {
     theme:{
-        preset: Aura,
+        preset: MyPreset,
         options: {
             darkModeSelector: false || 'none',
         }
